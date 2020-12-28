c7ab4c03c494e3f01f17db23fab3c0fbfc7c0306
jordansaphir <73350534+jordansaphir@users.noreply.github.com>
2020-12-28 10:05:50 +0000

    Update app.js

--- app.js
+++ app.js
@@ -55,3 +55,19 @@ const hideMobileMenu = () => {
 
 menuLinks.addEventListener('click', hideMobileMenu);
 navLogo.addEventListener('click', hideMobileMenu); 
+
+
+
+// Show tooltip on svg map
+function showTooltip(evt, text) {
+  let tooltip = document.getElementById("tooltip");
+  tooltip.innerHTML = text;
+  tooltip.style.display = "block";
+  tooltip.style.left = evt.pageX + 10 + 'px';
+  tooltip.style.top = evt.pageY + 10 + 'px';
+}
+
+function hideTooltip() {
+  var tooltip = document.getElementById("tooltip");
+  tooltip.style.display = "none";
+}
